3fc9f5d2864dbf5a2967ea3c23f58657b99337ef
Jihoon Kang <jihoonkang0829@gmail.com>
2022-05-08 14:34:25 -0500

    Update usersService.js

--- functions/usersService.js
+++ functions/usersService.js
@@ -1,11 +1,14 @@
 import { db } from "./firebase.js";
 import * as express from "express";
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, collection, query, where } from "firebase/firestore";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  getDocs(collection(db, "users"))
+  const usersDocReference = Object.keys(req.query).length === 0 ?
+    collection(db, "users") :
+    query(collection(db, "users"), where(Object.keys(req.query)[0], "==", Object.values(req.query)[0]));
+  getDocs(usersDocReference)
     .then((snapshot) => {
       const users = snapshot.docs.map((doc) => {
         return {
